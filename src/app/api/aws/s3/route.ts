dd637ba60aa783a204e36dec744c7d5d25c51149
zcemycl <lyc010197@gmail.com>
2025-05-26 23:21:05 +0100

    add uuid package

--- src/app/api/aws/s3/route.ts
+++ src/app/api/aws/s3/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(
+  // request: Request
+) {
   return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
 
-export async function POST(request: Request) {
+export async function POST(
+  // request: Request
+) {
   return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
