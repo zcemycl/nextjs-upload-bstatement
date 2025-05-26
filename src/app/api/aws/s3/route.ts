a9aa218318cac8d3a35745d0d287bd1eabbddd68
zcemycl <lyc010197@gmail.com>
2025-05-26 21:57:08 +0100

    update

--- src/app/api/aws/s3/route.ts
+++ src/app/api/aws/s3/route.ts
@@ -1,10 +1,9 @@
-import {NextResponse} from "next/server";
-
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return NextResponse.json({msg: "Hello world"} , {status: 200});
+  return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
 
 export async function POST(request: Request) {
-    return NextResponse.json({msg: "Hello world"} , {status: 200});
+  return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
--- src/app/api/claude/sonnet4/route.ts
+++ src/app/api/claude/sonnet4/route.ts
@@ -1,6 +1,5 @@
-import {NextResponse} from "next/server";
-
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    return NextResponse.json({msg: "Hello world"} , {status: 200});
+  return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
