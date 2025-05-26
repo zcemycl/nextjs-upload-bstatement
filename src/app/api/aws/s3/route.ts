f361cc4d104a7a48c0048ba817a3d50b2ec63fa9
zcemycl <lyc010197@gmail.com>
2025-05-27 00:09:18 +0100

    add s3 fake buckets

--- src/app/api/aws/s3/route.ts
+++ src/app/api/aws/s3/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-// request: Request
+  // request: Request
   return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
 
 export async function POST() {
-// request: Request
+  // request: Request
   return NextResponse.json({ msg: "Hello world" }, { status: 200 });
 }
