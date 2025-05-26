14084971fca3509db976a1bd743cfac078bc3796
zcemycl <lyc010197@gmail.com>
2025-05-26 21:55:34 +0100

    add apis

---
+++ src/app/api/aws/s3/route.ts
@@ -0,0 +1,10 @@
+import {NextResponse} from "next/server";
+
+
+export async function GET(request: Request) {
+    return NextResponse.json({msg: "Hello world"} , {status: 200});
+}
+
+export async function POST(request: Request) {
+    return NextResponse.json({msg: "Hello world"} , {status: 200});
+}
