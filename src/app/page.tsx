40d08484814ec3aa0f3eed8feebb77aa92115e77
zcemycl <lyc010197@gmail.com>
2025-05-26 17:39:59 +0100

    make basic frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
   return (
-    <div className="w-screen h-screen 
+    <div
+      className="w-screen h-screen 
       flex flex-col justify-center
       content-center items-center align-middle
-      p-10">
-        <div className="flex flex-col space-y-2 w-7/12">
+      p-10"
+    >
+      <div className="flex flex-col space-y-2 w-7/12">
         <h2 className="self-start text-2xl w-full animate-pulse">
-          Hello World, upload your bank statement here for scanning 😜</h2>
-        <input type="file" id="file"
+          Hello World, upload your bank statement here for scanning 😜
+        </h2>
+        <input
+          type="file"
+          id="file"
           className="*flex p-3
             rounded-lg
             bg-emerald-400
@@ -33,15 +38,15 @@ export default function Home() {
             console.log(e.target.files);
           }}
         />
-        <button className={
-          `p-3 bg-sky-300 hover:bg-sky-500 
+        <button
+          className={`p-3 bg-sky-300 hover:bg-sky-500 
           rounded-lg w-1/2 self-end text-black
           font-bold transition origin-top
-          ${ file !== null ? 'scale-y-100' : 'scale-y-0'}`}>
-            Submit
+          ${file !== null ? "scale-y-100" : "scale-y-0"}`}
+        >
+          Submit
         </button>
-        </div>
-        
+      </div>
     </div>
   );
 }
