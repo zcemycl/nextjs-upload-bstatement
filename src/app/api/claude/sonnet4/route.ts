c1af91691e02159a86be79b68bf61a8dba6134f0
zcemycl <lyc010197@gmail.com>
2025-05-27 15:07:12 +0100

    refactoring

--- src/app/api/claude/sonnet4/route.ts
+++ src/app/api/claude/sonnet4/route.ts
@@ -1,27 +1,6 @@
 import { NextResponse } from "next/server";
 import Anthropic from "@anthropic-ai/sdk";
-
-const prompt = `
-You are a bank assistant.
-- You must respond with ONLY a valid JSON object. 
-- Do not include any text, explanations, or formatting outside of the JSON. 
-- Your entire response should be parseable as JSON.
-- The response must start with { and end with }.
-Based on the given bank statement, can you extract the following informations, 
-- Name and address of the account holder
-- Latest Date of the document, if present
-- A list of all of the transactions in the document
-- The starting and ending balance of the statement. 
-Answer this with ONLY a structured json of a given interface below, 
-{
-"name": string,
-"address": string,
-"date": string,
-"transactions": string[],
-"starting-balance": number,
-"ending-balance": number,
-}
-`;
+import { prompt } from "@/constants";
 
 const anthropic = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
