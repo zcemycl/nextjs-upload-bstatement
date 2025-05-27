b06053bfe8213ef89cca893f91ece048ccfaab14
zcemycl <lyc010197@gmail.com>
2025-05-27 01:03:16 +0100

    init history page

--- src/app/history/[id]/page.tsx
+++ src/app/history/[id]/page.tsx
@@ -3,10 +3,6 @@ import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
 export default function History() {
-    const router = useRouter();
-    return (
-        <div className="p-10 flex flex-col">
-
-        </div>
-    )
+  const router = useRouter();
+  return <div className="p-10 flex flex-col"></div>;
 }
