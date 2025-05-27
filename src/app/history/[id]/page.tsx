2ac225f34c6b01f137208a01907c081739b5f20d
zcemycl <lyc010197@gmail.com>
2025-05-27 01:03:10 +0100

    init history page

---
+++ src/app/history/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+export default function History() {
+    const router = useRouter();
+    return (
+        <div className="p-10 flex flex-col">
+
+        </div>
+    )
+}
